07b9d53e35c31a5748c4531851bc7a5a1cd4f92c
Bdifh <mindravens@gmail.com>
2026-08-27 05:23:04 +0200

    MP000-a

--- shared/src/jvmMain/kotlin/com/example/interliplural_multiplatform/InterliPlural/DataModule/DataStore.kt
+++ shared/src/jvmMain/kotlin/com/example/interliplural_multiplatform/InterliPlural/DataModule/DataStore.kt
@@ -1,6 +1,8 @@
-package com.example.interliplural_multiplatform.InterliPlural.DataModule
+@file:JvmName("JvmDataStore")
 
 package com.example.interliplural_multiplatform.InterliPlural.DataModule
+
+import androidx.datastore.preferences.core.PreferencesFileSerializer
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.core.FileStorage
@@ -8,10 +10,10 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.PreferencesSerializer
 import java.io.File
 
-actual fun getDataStore(): DataStore<Preferences> {
+actual fun getDataStores(): DataStore<Preferences> {
     return createDataStore(
         storage = FileStorage(
-            serializer = PreferencesSerializer,
+            serializer = PreferencesFileSerializer,
             produceFile = {
                 File(
                     System.getProperty("user.home"),
