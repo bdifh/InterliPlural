044172a2ed1019d19e7040b3ba8244b24873d102
Bdifh <mindravens@gmail.com>
2026-08-28 16:31:45 +0200

    setback

--- shared/src/commonMain/kotlin/com/example/interliplural_multiplatform/InterliPlural/DataModule/DataStore.kt
+++ shared/src/commonMain/kotlin/com/example/interliplural_multiplatform/InterliPlural/DataModule/DataStore.kt
@@ -1,1 +1,10 @@
 package com.example.interliplural_multiplatform.InterliPlural.DataModule
+
+import androidx.datastore.preferences.core.stringPreferencesKey
+
+private val MEMBERS_KEY = stringPreferencesKey("members")
+private val FRONTING_MEMBERS_KEY = stringPreferencesKey("fronting_members")
+
+object AppDataStore {
+
+}
